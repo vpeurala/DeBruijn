49939ad089e324ef761517b3180d6ea43aaeef75
Ville Peurala <ville.peurala@reaktor.fi>
2015-02-27 16:49:10 +0200

    Both HUnit and QuickCheck tests

--- src/DeBruijn.hs
+++ src/DeBruijn.hs
@@ -7,24 +7,6 @@ import Data.Array.MArray
 import Data.Array.ST
 import Data.STRef
 
-main :: IO ()
-main = do
-  putStrLn "Foo"
-  let arr = arrayStuff
-  putStrLn $ show arr
-  putStrLn "Bar"
- 
-arrayStuff :: Array Int Int
-arrayStuff = runSTArray $ do
-  arr <- newArray (0, 4) 0
-  mutateArray arr
-  return arr
-
-mutateArray :: (MArray a e m, Ix i, Num e, Num i) => a i e -> m (a i e)
-mutateArray input = do
-  writeArray input 1 3
-  return input
-
 gen1 :: (Ord e, Enum e) => STRef s [e] -> STArray s Int e -> [e] -> Int -> Int -> Int -> ST s ()
 gen1 deBruijnSeq workingArray alphabet subSeqLen t period =
   if t > subSeqLen
@@ -44,14 +26,6 @@ gen1 deBruijnSeq workingArray alphabet subSeqLen t period =
       writeArray workingArray t j
       gen1 deBruijnSeq workingArray alphabet subSeqLen (t + 1) t)
 
-{--
-arrayThing deBruijnSeq alphabet subSeqLen = seq (runST $ do
-  workingArray <- newArray (0 :: Int, ((length alphabet) + 1) :: Int) (minimum alphabet)
-  gen1 deBruijnSeq workingArray alphabet subSeqLen 1 1
-  )
-  deBruijnSeq
---}
-
 calculateDeBruijnSeq :: (Enum a, Ord a) => [a] -> Int -> [a]
 calculateDeBruijnSeq []       _         = []
 calculateDeBruijnSeq alphabet subSeqLen = runST $ do
@@ -61,13 +35,3 @@ calculateDeBruijnSeq alphabet subSeqLen = runST $ do
   result <- readSTRef deBruijnSeq
   return $ result ++ take (subSeqLen - 1) result
 
--- deBruijnSeq alphabet subSeqLen = runST $ join $ calculateDeBruijnSeq alphabet subSeqLen
-
-{--
-deBruijn :: [a] -> Int -> Array Int a
-deBruijn alphabet k = runSTArray $ do
-  let zero         =  head alphabet
-  let alphabetSize =  length alphabet
-  workingArray     <- newArray (0, alphabetSize) zero
-  sequence         <- 
---}
---
+++ test/DeBruijnTest.hs
@@ -0,0 +1,84 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+module DeBruijnTest where
+
+import Data.List
+
+import Test.HUnit
+
+import Test.QuickCheck.Arbitrary
+import Test.QuickCheck.Gen
+import Test.QuickCheck.Modifiers
+
+import Test.Framework
+import Test.Framework.Providers.HUnit
+import Test.Framework.Providers.QuickCheck2
+
+import DeBruijn
+
+main :: IO ()
+main = defaultMain tests
+
+tests :: [Test.Framework.Test]
+tests =
+  [
+    testGroup "A De Bruijn sequence"
+    [
+      testProperty "Contains each n-length string from alphabet exactly once" prop_containsEveryNLengthString,
+      testCase "Contains each n-length string from alphabet exactly once" test_containsEveryNLengthString
+    ]
+  ]
+
+alphabet10        = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+deBruijnOf4From10  = calculateDeBruijnSeq alphabet10 4
+pickingsOf4From10 = pickings 4 alphabet10
+
+prop_containsEveryNLengthString :: SubSeqOf4FromAlphabet10 -> Bool
+prop_containsEveryNLengthString (SubSeqOf4FromAlphabet10 subSeq) =
+  occurrences subSeq deBruijnOf4From10 == 1
+
+test_containsEveryNLengthString =
+  let picksWithOccurrencesOtherThan1 = filter (\c -> occurrences c deBruijnOf4From10 /= 1) pickingsOf4From10
+      errors                         = map (\c -> "Pick " ++ show c ++ " should have occurred exactly once in sequence " ++ show deBruijnOf4From10 ++ ", but occurred " ++ show (occurrences c deBruijnOf4From10) ++ " times.") picksWithOccurrencesOtherThan1
+  in  [] @=? errors
+
+combinations :: Int -> [a] -> [[a]]
+combinations _ [] = []
+combinations 0 _ = [[]]
+combinations 1 xs = map (\x -> [x]) xs
+combinations n (x:xs) =
+  map (x:) (combinations (n - 1) xs)
+  ++
+  (combinations n xs)
+
+pickings :: Int -> [a] -> [[a]]
+pickings _ [] = []
+pickings 0 _  = [[]]
+pickings 1 xs = map (\x -> [x]) xs
+pickings n xs =
+  concatMap (\p -> map (\x -> (x:p)) xs) (pickings (n - 1) xs)
+
+occurrences :: (Eq a) => [a] -> [a] -> Int
+occurrences needle [] = 0
+occurrences needle haystack =
+  if (take (length needle) haystack) == needle
+  then 1 + (occurrences needle (tail haystack))
+  else occurrences needle (tail haystack)
+
+newtype Alphabet a = Alphabet [a] deriving (Eq, Ord, Read, Show)
+
+newtype SubSeqLen = SubSeqLen Int deriving (Enum, Eq, Integral, Num, Ord, Read, Real, Show)
+
+instance Functor Alphabet where
+  fmap f (Alphabet xs) = Alphabet (map f xs)
+
+instance (Arbitrary a) => Arbitrary (Alphabet a) where
+  arbitrary = Alphabet `fmap` (arbitrary `suchThat` (\xs -> length xs <= 5))
+
+instance Arbitrary SubSeqLen where
+  arbitrary = SubSeqLen `fmap` (arbitrary `suchThat` (\v -> v >= 0 && v <= 3))
+
+newtype SubSeqOf4FromAlphabet10 = SubSeqOf4FromAlphabet10 [Int] deriving (Eq, Ord, Read, Show)
+
+instance Arbitrary SubSeqOf4FromAlphabet10 where
+  arbitrary = SubSeqOf4FromAlphabet10 `fmap` (vectorOf 4 (elements alphabet10))
+
