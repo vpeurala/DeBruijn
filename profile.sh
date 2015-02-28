80ad4b9cebd17c5fcd8887a2592a4d521160f48b
Ville Peurala <ville.peurala@gmail.com>
2015-02-28 16:55:33 +0200

    Clean up profiling script a little

--- profile.sh
+++ profile.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-cabal clean && cd src && ghc -o Main -O2 --make -fforce-recomp -prof -caf-all -auto-all -rtsopts Main.hs && time ./Main +RTS -p -sstderr && rm Main && rm *.o && rm *.hi
+cabal clean && cd src && ghc -o Main -O2 --make -fforce-recomp -prof -caf-all -auto-all -rtsopts Main.hs && time ./Main +RTS -p -sstderr && rm Main *.o *.hi
 
