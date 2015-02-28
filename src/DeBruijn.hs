d471a96762f1716482e44282b7860be27effe017
Ville Peurala <ville.peurala@gmail.com>
2015-02-28 16:52:56 +0200

    Using vectors, a lot faster

--- src/DeBruijn.hs
+++ src/DeBruijn.hs
@@ -1,11 +1,14 @@
 module DeBruijn (deBruijnSequence, deBruijnString) where
 
 import Control.Monad
+import Control.Monad.Primitive
 import Control.Monad.ST
 import Data.Array.MArray
 import Data.Array.ST
 import Data.List (nub, sort)
 import Data.STRef
+import qualified Data.Vector as V
+import qualified Data.Vector.Mutable as VM
 
 deBruijnSequence :: (Enum a, Ord a) => [a] -> Int -> [a]
 deBruijnSequence alphabet subSequenceLength = calculateDeBruijnSeq (nub $ sort alphabet) subSequenceLength
@@ -14,23 +17,36 @@ deBruijnString :: (Enum a, Ord a, Show a) => [a] -> Int -> String
 deBruijnString alphabet subSequenceLength = concat $ map show $ deBruijnSequence alphabet subSequenceLength
 
 -- See "An Efficient Algorithm for Generating Necklaces with Fixed Density" by Joe Sawada and Frank Ruskey
-gen1 :: (Ord e, Enum e) => STRef s [e] -> STArray s Int e -> [e] -> Int -> Int -> Int -> ST s ()
+gen1 :: (Ord a) =>
+     STRef s (VM.MVector s a)
+     -> VM.MVector s a
+     -> [a]
+     -> Int
+     -> Int
+     -> Int
+     -> ST s ()
 gen1 deBruijnSeq workingArray alphabet subSeqLen t period =
   if t > subSeqLen
   then
     if (subSeqLen `mod` period == 0)
     then do
       deBruijnSeq' <- readSTRef deBruijnSeq
-      workingArray' <- getElems workingArray
-      writeSTRef deBruijnSeq (deBruijnSeq' ++ (drop (subSeqLen - period + 1) workingArray'))
+      let appendable = VM.drop (subSeqLen - period + 1) workingArray
+      let firstWritableIndex = VM.length deBruijnSeq'
+      let appendableLength = VM.length appendable
+      grown <- VM.grow deBruijnSeq' appendableLength
+      forM_ [0..(appendableLength - 1)] (\i -> do
+        item <- VM.read appendable i
+        VM.write grown (firstWritableIndex + i) item)
+      writeSTRef deBruijnSeq grown
     else do
       return ()
   else do
-    valueAtTMinusPeriod <- readArray workingArray (t - period)
-    writeArray workingArray t valueAtTMinusPeriod
+    valueAtTMinusPeriod <- VM.read workingArray (t - period)
+    VM.write workingArray t valueAtTMinusPeriod
     gen1 deBruijnSeq workingArray alphabet subSeqLen (t + 1) period
     forM_ (successors alphabet valueAtTMinusPeriod) (\j -> do
-      writeArray workingArray t j
+      VM.write workingArray t j
       gen1 deBruijnSeq workingArray alphabet subSeqLen (t + 1) t)
 
 successors :: (Ord a) => [a] -> a -> [a]
@@ -40,9 +56,15 @@ successors alphabet letter =
 calculateDeBruijnSeq :: (Enum a, Ord a) => [a] -> Int -> [a]
 calculateDeBruijnSeq []       _         = []
 calculateDeBruijnSeq alphabet subSeqLen = runST $ do
-  deBruijnSeq <- (newSTRef [])
-  workingArray <- newArray (0, subSeqLen) (minimum alphabet)
-  gen1 deBruijnSeq workingArray alphabet subSeqLen 1 1
-  result <- readSTRef deBruijnSeq
-  return $ result ++ take (subSeqLen - 1) result
+  deBruijnSeqWorking <- V.thaw $ V.fromList []
+  deBruijnSeqRef <- newSTRef deBruijnSeqWorking
+  workingArray <- VM.replicate (subSeqLen + 1) (minimum alphabet)
+  gen1 deBruijnSeqRef workingArray alphabet subSeqLen 1 1
+  deBruijnSeqFinal <- readSTRef deBruijnSeqRef
+  deBruijnSeqFrozen <- V.freeze deBruijnSeqFinal
+  let sequenceWithoutCycling = V.toList deBruijnSeqFrozen
+  let sequenceWithCycling = sequenceWithoutCycling ++ take (subSeqLen - 1) sequenceWithoutCycling
+  return sequenceWithCycling
+
+
 
